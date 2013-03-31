3299331ef9539b3ce809a354c2ea8fc17513e647
ankit.rokde <ankitrokdeonsns@gmail.com>
2013-03-31 10:34:20 +0530

    creating kshan for timezone not observing DST --ankit

--- spec/kshanSpec.js
+++ spec/kshanSpec.js
@@ -6,17 +6,18 @@ describe("kshan test suit", function(){
         expect(date.getUTCDay()).toEqual(kshan.day());
     };
 
-    var expectHoursMinutesSecondsAndMillisecondsToBeEqual = function(expectedDate, utcKshan) {
-        expect(expectedDate.getUTCHours()).toEqual(utcKshan.hours());
-        expect(expectedDate.getUTCMinutes()).toEqual(utcKshan.minutes());
-        expect(expectedDate.getUTCSeconds()).toEqual(utcKshan.seconds());
-        expect(expectedDate.getUTCMilliseconds()).toEqual(utcKshan.milliseconds());
+    var expectHoursMinutesSecondsAndMillisecondsToBeEqual = function(date, kshan) {
+        expect(date.getUTCHours()).toEqual(kshan.hours());
+        expect(date.getUTCMinutes()).toEqual(kshan.minutes());
+        expect(date.getUTCSeconds()).toEqual(kshan.seconds());
+        expect(date.getUTCMilliseconds()).toEqual(kshan.milliseconds());
     };
 
     it("should contruct utc kshan for today", function(){
         var utcKshan = Kshan();
         var expectedDate = new Date();
         expectDateMonthYearAndDayToBeEqual(expectedDate, utcKshan);
+        expect("Etc/UTC").toEqual(utcKshan.timezone());
     });
 
     it("should construct utc kshan for provided unix timestamp", function(){
@@ -24,17 +25,38 @@ describe("kshan test suit", function(){
         var expectedDate = new Date(1361618782000);
         expectDateMonthYearAndDayToBeEqual(expectedDate, utcKshan);
         expectHoursMinutesSecondsAndMillisecondsToBeEqual(expectedDate, utcKshan);
+        expect("Etc/UTC").toEqual(utcKshan.timezone());
     });
 
     it("should construct utc kshan for today when provided unix timestamp is undefined", function(){
         var utcKshan = Kshan(undefined);
         var expectedDate = new Date();
         expectDateMonthYearAndDayToBeEqual(expectedDate, utcKshan);
+        expect("Etc/UTC").toEqual(utcKshan.timezone());
     });
 
     it("should construct utc kshan for today when provided unix timestamp is null", function(){
         var utcKshan = Kshan(null);
         var expectedDate = new Date();
         expectDateMonthYearAndDayToBeEqual(expectedDate, utcKshan);
+        expect("Etc/UTC").toEqual(utcKshan.timezone());
+    });
+
+    it("should construct kshan from unix epoch and timezone for india timezone (not observing DST)", function(){
+        var kshan = Kshan(1361618782000, "Asia/Kolkata");
+        expect(kshan.date()).toEqual(23);
+        expect(kshan.month()).toEqual(1);
+        expect(kshan.year()).toEqual(2013);
+        expect(kshan.hours()).toEqual(16);
+        expect(kshan.minutes()).toEqual(56);
+        expect(kshan.seconds()).toEqual(22);
+        expect(kshan.milliseconds()).toEqual(0);
+        expect(kshan.day()).toEqual(6);
+        expect(kshan.timezone()).toEqual("Asia/Kolkata");
+    });
+
+    it("should set timezone to Etc/UTC when timezone is passed as null", function(){
+        var kshan = Kshan(1, null);
+        expect(kshan.timezone()).toEqual("Etc/UTC");
     });
 });
--- src/kshan.js
+++ src/kshan.js
@@ -1,9 +1,101 @@
-Kshan = (function(unixEpoch){
+Kshan = (function(unixEpoch, timezone){
+
+    var timezoneOffsetFor = {
+        'Etc/GMT+12' : '-720,0' ,
+        'Pacific/Pago_Pago' : '-660,0' ,
+        'America/Adak' : '-600,1' ,
+        'Pacific/Honolulu' : '-600,0' ,
+        'Pacific/Marquesas' : '-570,0' ,
+        'Pacific/Gambier' : '-540,0' ,
+        'America/Anchorage' : '-540,1' ,
+        'America/Los_Angeles' : '-480,1' ,
+        'Pacific/Pitcairn' : '-480,0' ,
+        'America/Phoenix' : '-420,0' ,
+        'America/Denver' : '-420,1' ,
+        'America/Guatemala' : '-360,0' ,
+        'America/Chicago' : '-360,1' ,
+        'Pacific/Easter' : '-360,1,s' ,
+        'America/Bogota' : '-300,0' ,
+        'America/New_York' : '-300,1' ,
+        'America/Caracas' : '-270,0' ,
+        'America/Halifax' : '-240,1' ,
+        'America/Santo_Domingo' : '-240,0' ,
+        'America/Santiago' : '-240,1,s' ,
+        'America/St_Johns' : '-210,1' ,
+        'America/Godthab' : '-180,1' ,
+        'America/Argentina/Buenos_Aires' : '-180,0' ,
+        'America/Montevideo' : '-180,1,s' ,
+        'Etc/GMT+2' : '-120,0' ,
+        'Etc/GMT+2' : '-120,1' ,
+        'Atlantic/Azores' : '-60,1' ,
+        'Atlantic/Cape_Verde' : '-60,0' ,
+        'Etc/UTC' : '0,0' ,
+        'Europe/London' : '0,1' ,
+        'Europe/Berlin' : '60,1' ,
+        'Africa/Lagos' : '60,0' ,
+        'Africa/Windhoek' : '60,1,s' ,
+        'Asia/Beirut' : '120,1' ,
+        'Africa/Johannesburg' : '120,0' ,
+        'Asia/Baghdad' : '180,0' ,
+        'Europe/Moscow' : '180,1' ,
+        'Asia/Tehran' : '210,1' ,
+        'Asia/Dubai' : '240,0' ,
+        'Asia/Baku' : '240,1' ,
+        'Asia/Kabul' : '270,0' ,
+        'Asia/Yekaterinburg' : '300,1' ,
+        'Asia/Karachi' : '300,0' ,
+        'Asia/Kolkata' : '330,0' ,
+        'Asia/Kathmandu' : '345,0' ,
+        'Asia/Dhaka' : '360,0' ,
+        'Asia/Omsk' : '360,1' ,
+        'Asia/Rangoon' : '390,0' ,
+        'Asia/Krasnoyarsk' : '420,1' ,
+        'Asia/Jakarta' : '420,0' ,
+        'Asia/Shanghai' : '480,0' ,
+        'Asia/Irkutsk' : '480,1' ,
+        'Australia/Eucla' : '525,0' ,
+        'Australia/Eucla' : '525,1,s' ,
+        'Asia/Yakutsk' : '540,1' ,
+        'Asia/Tokyo' : '540,0' ,
+        'Australia/Darwin' : '570,0' ,
+        'Australia/Adelaide' : '570,1,s' ,
+        'Australia/Brisbane' : '600,0' ,
+        'Asia/Vladivostok' : '600,1' ,
+        'Australia/Sydney' : '600,1,s' ,
+        'Australia/Lord_Howe' : '630,1,s' ,
+        'Asia/Kamchatka' : '660,1' ,
+        'Pacific/Noumea' : '660,0' ,
+        'Pacific/Norfolk' : '690,0' ,
+        'Pacific/Auckland' : '720,1,s' ,
+        'Pacific/Tarawa' : '720,0' ,
+        'Pacific/Chatham' : '765,1,s' ,
+        'Pacific/Tongatapu' : '780,0' ,
+        'Pacific/Apia' : '780,1,s' ,
+        'Pacific/Kiritimati' : '840,0'
+
+    };
     var _date;
-    if(unixEpoch !== undefined && unixEpoch !== null)
+    var _timezone;
+
+    var create = function(unixEpoch, timezone){
+        if(timezone === undefined || timezone === null)
+            timezone = "Etc/UTC";
+        _timezone = timezone;
+
+        var _timezoneOffsetKeys = timezoneOffsetFor[_timezone].split(",");
+        var timezoneOffsetInMinutes = parseInt(_timezoneOffsetKeys[0]) + 60*parseInt(_timezoneOffsetKeys[1]);
+
+        if (_timezone !== "Etc/UTC") {
+            unixEpoch += (timezoneOffsetInMinutes * 60000);
+        }
         _date = new Date(unixEpoch);
+    };
+
+    if(unixEpoch !== undefined && unixEpoch !== null)
+        create(unixEpoch, timezone);
     else
-        _date = new Date();
+        create((new Date()).getTime());
+
     return {
         date: function(){
             return _date.getUTCDate();
@@ -28,6 +120,10 @@ Kshan = (function(unixEpoch){
         },
         day:function(){
             return _date.getUTCDay();
+        },
+        timezone: function(){
+            return _timezone;
         }
     }
 });
+
