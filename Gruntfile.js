6f5825ff53b3b4541389a36a81187ca0eb0b6bc6
ankit rokde <ankitrokdeonsns@gmail.com>
2013-05-18 11:22:43 +0530

    added license, modified readme, updated package.json and Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,60 +11,6 @@ module.exports = function(grunt) {
       '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
       ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
     // Task configuration.
-    concat: {
-      options: {
-        banner: '<%= banner %>',
-        stripBanners: true
-      },
-      dist: {
-        src: ['lib/<%= pkg.name %>.js'],
-        dest: 'dist/<%= pkg.name %>.js'
-      }
-    },
-    uglify: {
-      options: {
-        banner: '<%= banner %>'
-      },
-      dist: {
-        src: '<%= concat.dist.dest %>',
-        dest: 'dist/<%= pkg.name %>.min.js'
-      }
-    },
-    jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        unused: true,
-        boss: true,
-        eqnull: true,
-        globals: {}
-      },
-      gruntfile: {
-        src: 'Gruntfile.js'
-      },
-      lib_test: {
-        src: ['lib/**/*.js', 'test/**/*.js']
-      }
-    },
-    nodeunit: {
-      files: ['test/**/*_test.js']
-    },
-    watch: {
-      gruntfile: {
-        files: '<%= jshint.gruntfile.src %>',
-        tasks: ['jshint:gruntfile']
-      },
-      lib_test: {
-        files: '<%= jshint.lib_test.src %>',
-        tasks: ['jshint:lib_test', 'nodeunit']
-      }
-    },
     jasmine : {
       // Your project's source files
       src : ['src/**/*.js', 'spec/**/*Helper.js'],
@@ -74,14 +20,8 @@ module.exports = function(grunt) {
     }
  });
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-jasmine');
   // Default task.
-  grunt.registerTask('default', ['jshint', 'nodeunit', 'concat', 'uglify']);
+  grunt.registerTask('default', ['jasmine']);
 
 };
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-1.Why not timezone.js or walltime.js
-they require tz database files to be included for set up
+Kshan.js
+========
+
+This is an attempt to represent datetime in various timezones and not be dependant upon your browser's timezone.
+Given standard constructor parameters for javascript date object and a timezone name Kshan object will represent the given datetimein that timezone.
+
+>e.g.
+>var kshan = Kshan(0, 'Asia/Kolkata')
+>kshan object represents Jan 1, 1970 05:30am India Standard Time
+
+The timezone information is gathered from Wikipedia, Olson timezone database files and (http://home.tiscali.nl/~t876506/TZworld.html#top)
+
+The goal is to support as many timezones as possible with emphasis on DST rules of recent past (preferably since Jan 1, 1970), present and future.
+
+Kshan means moment in Marathi language. 
